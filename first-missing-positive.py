ef84e4623beed50533dce59737e63ac0a67599ff
jeremyhorwich <jeremyahorwich@gmail.com>
2024-09-02 19:03:48 -0500

    Update first-missing-positive.py

--- first-missing-positive.py
+++ first-missing-positive.py
@@ -3,19 +3,25 @@
 #O(n) time, O(1) space
 
 def first_missing_positive(nums: list[int]) -> int:
-        max_length = (2**31)-1
-        seen = [0] * max_length
-        largest_possible = 1 + len(nums)
-        
-        for num in nums:
-            if num < 1 or num >= largest_possible:
-                continue
-            seen[num] = 1
-
-        for i in range(1,largest_possible + 1):
-            if seen[i] == 0:
-                return i
+    i = 0
+    while i < len(nums):
+        if nums[i] <= 0 or nums[i] > len(nums):
+            i += 1
+            continue
+        if nums[i] == i + 1:
+            i += 1
+            continue
+        index_to_swap_to = nums[i] - 1
+        if nums[i] == nums[index_to_swap_to]:
+            i += 1
+            continue
+        nums[i], nums[index_to_swap_to] = nums[index_to_swap_to], nums[i]
+    for i in range(0, len(nums)):
+        if nums[i] != i + 1:
+            return i + 1
+    return len(nums) + 1
     
 print(first_missing_positive([1,2,0]))
+print(first_missing_positive([1]))
 print(first_missing_positive([3,4,-1,1]))
 print(first_missing_positive([7,8,9,11,12]))
